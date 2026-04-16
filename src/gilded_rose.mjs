a615d09c2522cd44c187dd92af585ea52b056876
Sami Heinimo <sami.heinimo@helsinki.fi>
2026-04-16 15:34:34 +0300

    Start refactoring use constant name instead of this.items[i]

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -13,10 +13,12 @@ export class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
+      const item = this.items[i];
+
+      if (item.name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality > 0) {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.quality -= 1;
           }
         }
       } else {
